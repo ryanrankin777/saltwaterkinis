df293549d1968ece882db57cb8a7568536b19db0
ryanrankin777 <saltwaterkinis@gmail.com>
2025-01-08 11:38:31 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function shopNow() {
+  alert("erdirecting to our shop page! {coming soon!}");
+}
+
+document.getElementbyId("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("thanks for reaching out! we will get back to you soon!");
+});
